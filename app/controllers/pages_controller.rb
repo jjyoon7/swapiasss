073bad5137ac7a6d534a6d0dcd14a2f1ef1048bc
Jay Yoon <jay.yoon7@gmail.com>
2016-08-04 17:12:35 +0200

    update new method swapi data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,18 @@ class PagesController < ApplicationController
   require "swapi"
 
   def home
-    @data_from_api = Swapi.get_person(id) || Swapi.get_planet(id)
+    @data_from_api = swapi_data(id)
   end
 
+  def swapi_data(id)
+    Swapi.get_person(people_id) ||
+    Swapi.get_planet(planet_id) ||
+    Swapi.get_starship(starship_id) ||
+    Swapi.get_vehicle(vehicle_id) ||
+    Swapi.get_film(film_id) ||
+    Swapi.get_all("resource_type")
+  end
+
+
 end
+
